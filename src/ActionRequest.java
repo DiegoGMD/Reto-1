88881eda24d5c11491b517e8443547d1cb14e069
Tanotom <sebgonosp@alu.edu.gva.es>
2023-12-14 18:47:51 +0100

    Getters and setters ActionRequest

--- src/ActionRequest.java
+++ src/ActionRequest.java
@@ -5,7 +5,7 @@ public class ActionRequest {
     private String title;
     private String description;
     private String article;
-
+//------------------------------------Constructor-----------------------------------------//
     public ActionRequest (int id_request, int id_category, String id_user, String title,
                           String description, String article) {
         this.id_request = id_request;
@@ -13,6 +13,55 @@ public class ActionRequest {
         this.id_user = id_user;
         this.title = title;
         this.description = description;
+        this.article = article;
+    }
+//------------------------------------Getters---------------------------------------------//
+
+    public int getId_request() {
+        return id_request;
+    }
+
+    public int getId_category() {
+        return id_category;
+    }
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+//-----------------------------------Setters----------------------------------------------//
+    public void setId_request(int id_request) {
+        this.id_request = id_request;
+    }
+
+    public void setId_category(int id_category) {
+        this.id_category = id_category;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setArticle(String article) {
         this.article = article;
     }
 }
