f422d3d6ed1dd1a6ae32fb76c7c60aef79adac3e
Tanotom <sebgonosp@alu.edu.gva.es>
2023-12-15 16:03:21 +0100

    Commit sebastian viernes 1.2

--- src/MainPruebaUser.java
+++ src/MainPruebaUser.java
@@ -106,7 +106,8 @@ public class MainPruebaUser {
                 System.out.println("Choose the code of the Hardware involved");
                 System.out.println(CsvHandler.makeInventory());
                 String cod_Article = Global.inputKeyboard.nextLine();
-
+                ActionRequest peitition = new ActionRequest(IDPetition, cod_category, user.getDNI(),title, description,cod_Article);
+                System.out.println(peitition);
             } else if (option.equals("3")) {
                 return false;
             }
